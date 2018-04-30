4bcff5d0218ccabd3a40e3f9bd7018dd0f0244c2
Adrienne Yao <adrienne.yao@icloud.com>
2018-04-30 20:33:02 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,16 @@
+class Backer
+  attr_accessor :name, :backed_projects
+
+ def initialize(name)
+   @name = name
+   @backed_projects = []
+ end
+
+ def back_project(project)
+   if ! @backed_projects.detect { |x| x == project }
+     @backed_projects << project
+     project.add_backer(self)
+   end
+ end
+
+end
